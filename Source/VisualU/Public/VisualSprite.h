cf8e947c17ffc057e98727f8cc76a55abcd0be63
Razdvizh <shustov-jenja@rambler.ru>
2023-06-07 03:26:51 -0500

    Visual Sprite

--- Source/VisualU/Public/VisualSprite.h
+++ Source/VisualU/Public/VisualSprite.h
@@ -19,7 +19,6 @@ class VISUALU_API UVisualSprite : public UUserWidget
 public:
 	UVisualSprite(const FObjectInitializer& ObjectInitializer);
 
-	/*TODO: Make it implementable in Blueprints; think of more generic version*/
 	UFUNCTION(BlueprintCallable, Category = "Visual Sprite")
 	virtual void AssignExpressions(const TArray<TSoftObjectPtr<UPaperFlipbook>>& InExpressions);
 };
