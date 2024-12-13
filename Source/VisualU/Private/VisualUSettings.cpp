b2b46e0ba46cdbcba208013802915f6e5562db0b
Razdvizh <shustov-jenja@rambler.ru>
2024-12-12 18:20:05 -0600

    Fix macro placement

--- Source/VisualU/Private/VisualUSettings.cpp
+++ Source/VisualU/Private/VisualUSettings.cpp
@@ -11,4 +11,7 @@ UVisualUSettings::UVisualUSettings(const FObjectInitializer& ObjectInitializer)
 	AParameterName(TEXT("Transition 1")),
 	BParameterName(TEXT("Transition 2"))
 {
+#if WITH_EDITORONLY_DATA
+	ScenarioFlagsNameOverrides = TArray<FString>();
+#endif
 }
