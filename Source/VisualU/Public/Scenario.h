8baf87be13aa9cdc188f31badfd64b9abaadd647
Razdvizh <shustov-jenja@rambler.ru>
2025-01-06 12:42:58 -0600

    1.3 release

--- Source/VisualU/Public/Scenario.h
+++ Source/VisualU/Public/Scenario.h
@@ -249,7 +249,7 @@ public:
 		String += FString::Printf(TEXT("Line: %s\n"), Line.IsEmpty() ? TEXT("") : *Line.ToString());
 		String += FString::Printf(TEXT("Music: %s\n"), Sound.IsNull() ? TEXT("None") : *Sound.GetAssetName());
 		String += (Background.ToString() + TEXT("\n"));
-		String += FString::Printf(TEXT("Flags: %s\n"), *StaticEnum<EScenarioMetaFlags>()->GetValueOrBitfieldAsAuthoredNameString(Flags));
+		String += FString::Printf(TEXT("Flags: %s\n"), *StaticEnum<EScenarioMetaFlags>()->GetValueOrBitfieldAsString(Flags));
 
 		if (!SpritesParams.IsEmpty())
 		{
